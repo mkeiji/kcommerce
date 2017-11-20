10ecd240613203875d62fc0baf86b55b558bb0d2
keiji <keiji@linux.com>
2017-11-20 01:32:20 -0600

    home controller updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
+  
+  # home page index action
+  # @param 
+  # @return void
   def index
+
     # get products to be displayed
     @all_cars = Car.all
     @cars_to_be_displayed = Car.order("RAND()").limit(10)
-  end
-end
+
+  end#end of index ACTION
+
+end#--end of HomeController CLASS
