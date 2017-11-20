af674d2d838d2123e930dd60fdd2fe4da52f465e
keiji <keiji@linux.com>
2017-11-20 01:24:02 -0600

    getting random cars to be displayed in homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    # get all products
+    # get products to be displayed
     @all_cars = Car.all
+    @cars_to_be_displayed = Car.order("RAND()").limit(10)
   end
 end
